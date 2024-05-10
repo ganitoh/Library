f6193d951436ed453c428ac30e31a3662bddc07c
Vadim <vadim.gan2018@yandex.ru>
2024-05-10 20:12:16 +0300

    add extensions for application layer

---
+++ Library.Application/DIExtensions.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Library.Application.CQRS;
+using Library.Application.CQRS.Authors.Commands.CreateAuthor;
+using Library.Application.CQRS.Books.Commands.CreateBook;
+using Library.Application.CQRS.Users.Commands.CreateUser;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Library.Application
+{
+    public static class DIExtensions
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+
+            services.AddAutoMapper(typeof(CQRSMapperProfile));
+
+            services.AddScoped<IValidator<CreateUserCommand>,CreateUserCommandValidation>();
+            services.AddScoped<IValidator<CreateBookCommand>,CreateBookCommandValidation>();
+            services.AddScoped<IValidator<CreateAuthorCommand>,CreateAuthorCommandValidation>();
+        }
+    }
+}
